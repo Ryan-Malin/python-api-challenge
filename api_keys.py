d5dc7ac20256de462d315937666587b06a1b6379
Ryan Malin <76079150+spoomalin@users.noreply.github.com>
2021-07-21 14:19:04 -0400

    21July2021

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "c5779a215d57f746e1592fcc7f7b17b7"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCCbTVJaf9F6pPkDLBMQfTbwNXtOgMxve4"
